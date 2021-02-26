b77a233260dea8960d96e7b6ea1557c3550b03c3
邹兰 <zoulan@cmbc.com.cn>
2021-02-26 09:40:54 +0800

    自定义组件

--- examples/App.vue
+++ examples/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <x-button type="primary">button</x-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入组件库
+import fe_vui from '../packages'
+
+// 注册组件库
+Vue.use(fe_vui)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
